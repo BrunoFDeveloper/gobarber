5dbf9ee5caf1660b199348ba73dce0eecdae261f
Bruno Alencar <berenxd@gmail.com>
2020-01-30 21:04:16 -0300

    First

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import authMiddleware from './app/middlewares/auth';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import FileController from './app/controllers/FileController';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/users', UserController.index);
+routes.put('/user', UserController.update);
+routes.delete('/user/:id', UserController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
